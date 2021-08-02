cd4d1df1f73c9e04b53f17018ee72a117d19b50f
JivaniR <87755708+JivaniR@users.noreply.github.com>
2021-08-02 11:31:36 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var student;
+var age;
+var grade;
+
+function setup() {
+  createCanvas(400,400);
+  student= new Student("John",11,6);
+  student.display()
+}
+
+function draw() 
+{
+  background(30);
+ 
+}
